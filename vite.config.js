4578006c9260e7cf0f656157cd28d8cc498ade5f
Matthew Carroll <matthew.finn.carroll@gmail.com>
2025-07-15 21:34:47 -0700

    Deploy to github pages

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This is the base path of your site.
+  // It should be the name of your GitHub repository.
+  base: '/otp-extractor-web/',
+});
