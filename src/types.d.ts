866ea657726529c31b3e60414644c46cbbd2a319
Matthew Carroll <matthew.finn.carroll@gmail.com>
2025-07-19 16:30:03 -0700

    add otpparam interface to types file

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,17 @@ export interface OtpData {
   secret: string;
   issuer: string;
   type: string;
-  counter: number | '';
+  counter: number | "";
   url: string;
+}
+
+// Define a more specific type for the raw OTP data from the protobuf payload.
+export interface MigrationOtpParameter {
+  secret: Uint8Array;
+  name: string;
+  issuer: string;
+  algorithm: number; // ALGORITHM_UNSPECIFIED (0), SHA1 (1)
+  digits: number; // DIGITS_UNSPECIFIED (0), SIX (1), EIGHT (2)
+  type: number; // TYPE_UNSPECIFIED (0), HOTP (1), TOTP (2)
+  counter: number;
 }
